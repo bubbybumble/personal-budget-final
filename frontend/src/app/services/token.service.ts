e3c5944fbe3cf0b9e9cdb1f5ad32d9c458748fae
bubbybumble <bubbybumble@gmail.com>
2023-12-11 18:16:56 -0500

    implemented token expiration

--- frontend/src/app/services/token.service.ts
+++ frontend/src/app/services/token.service.ts
@@ -1,6 +1,8 @@
 // token.service.ts
 import { Injectable } from '@angular/core';
 import { getAuth } from "firebase/auth";
+import { timer } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
@@ -8,6 +10,44 @@ import { getAuth } from "firebase/auth";
 export class TokenService {
   private token: string | null = null;
 
+  private timeLeft = 120;
+  constructor(private router: Router) {}
+  startTimer() {
+    const source = timer(1000, 2000);
+    const abc = source.subscribe(val => {
+      if (this.token) {
+        
+        this.timeLeft -= val;
+        if (this.timeLeft <= 0) {
+          this.setToken('none');
+          this.router.navigate(['/auth']);
+          getAuth().signOut();
+          this.resetTimer();
+          abc.unsubscribe();
+          window.alert("You have been signed out due to inactivity");
+        } else if (this.timeLeft <= 20) {
+          this.confirmDialogue()
+          .then(() => {
+            this.resetTimer();
+            abc.unsubscribe();
+            this.startTimer();
+          });
+        }
+      } else {
+        this.resetTimer();
+        abc.unsubscribe();
+      }
+      
+    });
+  }
+
+  confirmDialogue() {
+    return new Promise(function (resolve, reject) {
+      let confirmed = window.confirm('Logging out in 20 seconds, do you wish to stay logged in?');
+
+      return confirmed ? resolve(true) : reject(false);
+    })
+  }
   setToken(token: string): void {
     if (token === "none") {
       this.token = null;
@@ -15,6 +55,8 @@ export class TokenService {
     } else {
       this.token = token;
       localStorage.setItem("token", this.token);
+      this.startTimer();
+      this.resetTimer();
     }
     
   }
@@ -23,11 +65,16 @@ export class TokenService {
     if (this.token === null || this.token === undefined) {
       this.token = localStorage.getItem("token");
     }
+    this.resetTimer();
     return this.token;
   }
 
   clearToken(): void {
     this.token = null;
+  }
+
+  resetTimer() {
+    this.timeLeft = 120;
   }
 
   isLoggedIn(): boolean {
